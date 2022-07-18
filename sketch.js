edd86f7550be6e1aff7090b25608843c719e7196
vale630 <72229922+vale630@users.noreply.github.com>
2022-07-18 14:03:49 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,11 +3,13 @@ var explorador, exploradorImg;
 var dinosaurio, dinosaurioImg;
 var tucan, tucanImg;
 var baloo, balooImg;
+var puerta, puertaImg;
+var trampolin, trampolinImg;
 
 
 function preload(){
 
-bgImg = loadImage  ("Img/fondo.png");  
+bgImg = loadImage ("Img/fondo.png");  
 exploradorImg = loadImage ("Img/explorador.png"); 
 
 
@@ -19,16 +21,29 @@ createCanvas (windowWidth, windowHeight);
 //Imagen de fondo
 bg = createSprite (displayWidth/2-20, displayHeight/2-40,20,20);
 bg.addImage(bgImg);
+bg.velocityX = -4;
 bg.scale = 1.3;
 
-explorador = createSprite (130,200,30,30); 
-explorador.addAnimation("explorador.png");
+
+//creamos al explorador
+explorador = createSprite (100,530,30,30); 
+explorador.addAnimation ("explorador.png",exploradorImg);
+explorador.velocityY = 4; 
 explorador.scale = 0.25;
 
 }
 
 function draw(){
- drawSprites();
+    background (0);
+ explorador.velocityY = 4;
 
-  
+ explorador.y = World.mouseY;
+
+//codigo para fondo
+if (bg.x<0){
+    bg.x = bg.width/8;
 }
+
+drawSprites();
+}
+
